3bbd089a4b9023f29f89f961a5efd58d280ba617
Pablo Garrido <pablogs9@gmail.com>
2022-04-11 14:53:26 +0200

    Update
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- platform_code/arduino/wifi_nina/micro_ros_transport.h
+++ platform_code/arduino/wifi_nina/micro_ros_transport.h
@@ -3,8 +3,8 @@
 #include <WiFiUdp.h>
 
 struct micro_ros_agent_locator {
-	IPAddress address;
-	int port;
+    IPAddress address;
+    int port;
 };
 
 static inline void set_microros_wifi_transports(char * ssid, char * pass, IPAddress agent_ip, uint16_t agent_port){
@@ -12,16 +12,16 @@ static inline void set_microros_wifi_transports(char * ssid, char * pass, IPAddr
       delay(500);
     }
 
-	static struct micro_ros_agent_locator locator;
-	locator.address = agent_ip;
-	locator.port = agent_port;
+    static struct micro_ros_agent_locator locator;
+    locator.address = agent_ip;
+    locator.port = agent_port;
 
-	rmw_uros_set_custom_transport(
-		false,
-		(void *) &locator,
-		platformio_transport_open,
-		platformio_transport_close,
-		platformio_transport_write,
-		platformio_transport_read
-	);
+    rmw_uros_set_custom_transport(
+        false,
+        (void *) &locator,
+        platformio_transport_open,
+        platformio_transport_close,
+        platformio_transport_write,
+        platformio_transport_read
+    );
 }
