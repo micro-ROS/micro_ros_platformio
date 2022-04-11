f09555a9db1794dee7c2640cb200fef45893edc2
Pablo Garrido <pablogs9@gmail.com>
2022-04-11 16:05:27 +0200

    Fix header
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- platform_code/micro_ros_platformio.h
+++ platform_code/micro_ros_platformio.h
@@ -4,23 +4,23 @@
 
 #define __attribute__(x)
 
+#include <uxr/client/transport.h>
+#include <rmw_microros/rmw_microros.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-
-#include <uxr/client/transport.h>
-#include <rmw_microros/rmw_microros.h>
 
 bool platformio_transport_open(struct uxrCustomTransport * transport);
 bool platformio_transport_close(struct uxrCustomTransport * transport);
 size_t platformio_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
 size_t platformio_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
 
-#include <micro_ros_transport.h>
-
 #ifdef __cplusplus
 }
 #endif
+
+#include <micro_ros_transport.h>
 
 #endif  // MICRO_ROS_PLATFORMIO
