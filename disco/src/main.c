eac37f75197fb0559431e9e9e0bc30552e11cd4e
Pablo Garrido <pablogs9@gmail.com>
2022-04-13 20:09:40 +0200

    Update
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- disco/src/main.c
+++ disco/src/main.c
@@ -1,33 +1,84 @@
-#include <errno.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/stm32/exti.h>
-#include <libopencm3/stm32/flash.h>
-#include <libopencm3/stm32/gpio.h>
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 
-
-volatile static uint8_t a[1200];
-
-
-__attribute__((constructor))
-void fun(void)
+static void clock_setup(void)
 {
-    for (size_t i = 0; i < 1200; i++)
-    {
-        a[i]= 12;
-    }
+	/* Enable GPIOD clock for LED & USARTs. */
+	rcc_periph_clock_enable(RCC_GPIOD);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Enable clocks for USART3. */
+	rcc_periph_clock_enable(RCC_USART3);
 }
 
-int main(void) {
+static void usart_setup(void)
+{
+	/* Setup USART3 parameters. */
+	usart_set_baudrate(USART3, 115200);
+	usart_set_databits(USART3, 8);
+	usart_set_stopbits(USART3, USART_STOPBITS_1);
+	usart_set_mode(USART3, USART_MODE_TX);
+	usart_set_parity(USART3, USART_PARITY_NONE);
+	usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
 
-    for (size_t i = 0; i < 1200; i++)
-    {
-        printf("%d\n", a[i]);
-    }
+	/* Finally enable the USART. */
+	usart_enable(USART3);
+}
 
-    return 0;
+static void gpio_setup(void)
+{
+	/* Setup GPIO pin GPIO12 on GPIO port D for LED. */
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+
+	/* Setup GPIO pins for USART3 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+
+	/* Setup USART3 TX pin as alternate function. */
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+}
+
+int main(void)
+{
+	int i, j = 0, c = 0;
+
+	clock_setup();
+	gpio_setup();
+	usart_setup();
+
+	/* Blink the LED (PD12) on the board with every transmitted byte. */
+	while (1) {
+		gpio_toggle(GPIOD, GPIO12);	/* LED on/off */
+		usart_send_blocking(USART3, c + '0'); /* USART3: Send byte. */
+		c = (c == 9) ? 0 : c + 1;	/* Increment c. */
+		if ((j++ % 80) == 0) {		/* Newline after line full. */
+			usart_send_blocking(USART3, '\r');
+			usart_send_blocking(USART3, '\n');
+		}
+		for (i = 0; i < 3000000; i++) {	/* Wait a bit. */
+			__asm__("NOP");
+		}
+	}
+
+	return 0;
 }
