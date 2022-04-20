cd20e959f71d185ad7c4bbb77c17585d6e9ed496
acuadros95 <acuadros1995@gmail.com>
2022-04-20 17:10:11 +0200

    Add clean stage and stderr output

--- extra_script.py
+++ extra_script.py
@@ -1,12 +1,13 @@
 Import("env")
 import os, sys
+import shutil
 
 ##############################
 #### Install dependencies ####
 ##############################
 
 pip_packages = [x.split("==")[0] for x in os.popen('{} -m pip freeze'.format(env['PYTHONEXE'])).read().split('\n')]
-required_packages = ["catkin-pkg", "lark-parser", "empy", "colcon-common-extensions", "importlib-resources", "pyyaml"]
+required_packages = ["catkin-pkg", "lark-parser", "empy", "colcon-common-extensions", "importlib-resources", "pyyaml", "pytz", "markupsafe==2.0.1"]
 if all([x in pip_packages for x in required_packages]):
     print("All required Python pip packages are installed")
 
@@ -36,10 +37,10 @@ boards_metas = {
 }
 
 project_options = env.GetProjectConfig().items(env=env["PIOENV"], as_dict=True)
+main_path = os.path.realpath(".")
 global_env = DefaultEnvironment()
 board = env['BOARD']
 framework = env['PIOFRAMEWORK'][0]
-main_path = os.path.realpath(".")
 extra_packages_path = "{}/extra_packages".format(env['PROJECT_DIR'])
 
 selected_board_meta = boards_metas[board] if board in boards_metas else "colcon.meta"
@@ -56,69 +57,92 @@ microros_user_meta = global_env.BoardConfig().get("microros_user_meta", "")
 # Do not include build folder
 env['SRC_FILTER'] += ' -<build/include/*>'
 
+################################
+#### Library custom targets ####
+################################
+
+def clean_microros_callback(*args, **kwargs):
+    library_path = main_path + '/libmicroros'
+    build_path = main_path + '/build'
+
+    # Delete library and build folders
+    shutil.rmtree(library_path, ignore_errors=True)
+    shutil.rmtree(build_path, ignore_errors=True)
+
+    print("micro-ROS library cleaned!")
+    os._exit(0)
+
+env.AddPlatformTarget("clean_microros", None, clean_microros_callback)
 
 #################################
 #### Build micro-ROS library ####
 #################################
 
-print("Configuring {} with transport {}".format(board, microros_transport))
+def build_microros(*args, **kwargs):
+    print("Configuring {} with transport {}".format(board, microros_transport))
 
-cmake_toolchain = library_builder.CMakeToolchain(
-    main_path + "/platformio_toolchain.cmake",
-    env['CC'],
-    env['CXX'],
-    env['AR'],
-    "{} {} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='".format(' '.join(env['CFLAGS']), ' '.join(env['CCFLAGS'])),
-    "{} {} -fno-rtti -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='".format(' '.join(env['CXXFLAGS']), ' '.join(env['CCFLAGS']))
-)
+    cmake_toolchain = library_builder.CMakeToolchain(
+        main_path + "/platformio_toolchain.cmake",
+        env['CC'],
+        env['CXX'],
+        env['AR'],
+        "{} {} -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='".format(' '.join(env['CFLAGS']), ' '.join(env['CCFLAGS'])),
+        "{} {} -fno-rtti -DCLOCK_MONOTONIC=0 -D'__attribute__(x)='".format(' '.join(env['CXXFLAGS']), ' '.join(env['CCFLAGS']))
+    )
 
-builder = library_builder.Build(library_folder=main_path, packages_folder=extra_packages_path)
-builder.run('{}/metas/{}'.format(main_path, selected_board_meta), cmake_toolchain.path, microros_user_meta)
+    builder = library_builder.Build(library_folder=main_path, packages_folder=extra_packages_path)
+    builder.run('{}/metas/{}'.format(main_path, selected_board_meta), cmake_toolchain.path, microros_user_meta)
 
-#######################################################
-#### Add micro-ROS library/includes to environment ####
-#######################################################
+    #######################################################
+    #### Add micro-ROS library/includes to environment ####
+    #######################################################
 
-# Add library
-if (board == "portenta_h7_m7" or board == "nanorp2040connect"):
-    # Workaround for including the library in the linker group
-    #   This solves a problem with duplicated symbols in Galactic
-    global_env["_LIBFLAGS"] = "-Wl,--start-group " + global_env["_LIBFLAGS"] + " -l{} -Wl,--end-group".format(builder.library_name)
-else:
-    global_env.Append(LIBS=[builder.library_name])
+    # Add library
+    if (board == "portenta_h7_m7" or board == "nanorp2040connect"):
+        # Workaround for including the library in the linker group
+        #   This solves a problem with duplicated symbols in Galactic
+        global_env["_LIBFLAGS"] = "-Wl,--start-group " + global_env["_LIBFLAGS"] + " -l{} -Wl,--end-group".format(builder.library_name)
+    else:
+        global_env.Append(LIBS=[builder.library_name])
 
-# Add library path
-global_env.Append(LIBPATH=[builder.library_path])
+    # Add library path
+    global_env.Append(LIBPATH=[builder.library_path])
 
-# Add required defines
-global_env.Append(CPPDEFINES=[("CLOCK_MONOTONIC", 1)])
+    # Add required defines
+    global_env.Append(CPPDEFINES=[("CLOCK_MONOTONIC", 1)])
 
-# Add micro-ROS include path
-global_env.Append(CPPPATH=[main_path + "/build/libmicroros/include"])
+    # Add micro-ROS include path
+    global_env.Append(CPPPATH=[main_path + "/libmicroros/include"])
 
-# Add micro-ROS include path to library include path
-env.Append(CPPPATH=[main_path + "/build/libmicroros/include"])
+    # Add micro-ROS include path to library include path
+    env.Append(CPPPATH=[main_path + "/libmicroros/include"])
 
-# Add platformio library general include path
-global_env.Append(CPPPATH=[
-    main_path + "/platform_code",
-    main_path + "/platform_code/{}/{}".format(framework, microros_transport)])
+    # Add platformio library general include path
+    global_env.Append(CPPPATH=[
+        main_path + "/platform_code",
+        main_path + "/platform_code/{}/{}".format(framework, microros_transport)])
 
-# Add platformio library general to library include path
-env.Append(CPPPATH=[
-    main_path + "/platform_code",
-    main_path + "/platform_code/{}/{}".format(framework, microros_transport)])
+    # Add platformio library general to library include path
+    env.Append(CPPPATH=[
+        main_path + "/platform_code",
+        main_path + "/platform_code/{}/{}".format(framework, microros_transport)])
 
-# Add micro-ROS defines to user application
-global_env.Append(CPPDEFINES=[('MICRO_ROS_TRANSPORT_{}_{}'.format(framework.upper(), microros_transport.upper()), 1)])
-global_env.Append(CPPDEFINES=[('MICRO_ROS_DISTRO_ {} '.format(microros_distro.upper()), 1)])
+    # Add micro-ROS defines to user application
+    global_env.Append(CPPDEFINES=[('MICRO_ROS_TRANSPORT_{}_{}'.format(framework.upper(), microros_transport.upper()), 1)])
+    global_env.Append(CPPDEFINES=[('MICRO_ROS_DISTRO_ {} '.format(microros_distro.upper()), 1)])
 
-# Add platformio library for Arduino framework
-if 'arduino' == framework:
-    # Include path for Arduino framework
-    global_env.Append(CPPPATH=[main_path + "/arduino"])
-    # Clock implementation for Arduino framework
-    env['SRC_FILTER'] += ' +<platform_code/arduino/clock_gettime.cpp>'
+    # Add platformio library for Arduino framework
+    if 'arduino' == framework:
+        # Include path for Arduino framework
+        global_env.Append(CPPPATH=[main_path + "/arduino"])
+        # Clock implementation for Arduino framework
+        env['SRC_FILTER'] += ' +<platform_code/arduino/clock_gettime.cpp>'
 
-# Add transport sources according to the framework and the transport
-env['SRC_FILTER'] += ' +<platform_code/{}/{}/micro_ros_transport.cpp>'.format(framework, microros_transport)
+    # Add transport sources according to the framework and the transport
+    env['SRC_FILTER'] += ' +<platform_code/{}/{}/micro_ros_transport.cpp>'.format(framework, microros_transport)
+
+
+from SCons.Script import COMMAND_LINE_TARGETS
+
+if "clean_microros" not in COMMAND_LINE_TARGETS:
+    build_microros()
