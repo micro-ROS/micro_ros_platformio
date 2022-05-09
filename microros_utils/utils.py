edff09e192c37c73850104b0cc66532909e6bb1d
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2022-05-09 07:56:56 +0200

    Add clean to source ROS2 (#25)
    * Add clean to source ROS2
    
    * Clean PR

--- microros_utils/utils.py
+++ microros_utils/utils.py
@@ -1,7 +1,8 @@
 import subprocess
 
-def run_cmd(command):
+def run_cmd(command, env=None):
     return subprocess.run(command,
         capture_output = True,
         shell = True,
+        env=env
     )
