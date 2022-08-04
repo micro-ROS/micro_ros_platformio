405700324b4bd1feaf6ab9cc218a818e621302a7
Pablo Garrido <pablogs9@gmail.com>
2022-08-04 09:03:51 +0200

    Fix Arduino with POSIX
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- platform_code/arduino/clock_gettime.cpp
+++ platform_code/arduino/clock_gettime.cpp
@@ -3,19 +3,23 @@
 #include <sys/time.h>
 #define micro_rollover_useconds 4294967295
 
+#ifndef WITH_POSIX
+
 extern "C" int clock_gettime(clockid_t unused, struct timespec *tp)
 {
-(void)unused;
-static uint32_t rollover = 0;
-static uint32_t last_measure = 0;
+    (void)unused;
+    static uint32_t rollover = 0;
+    static uint32_t last_measure = 0;
 
-uint32_t m = micros();
-rollover += (m < last_measure) ? 1 : 0;
+    uint32_t m = micros();
+    rollover += (m < last_measure) ? 1 : 0;
 
-uint64_t real_us = (uint64_t) (m + rollover * micro_rollover_useconds);
-tp->tv_sec = real_us / 1000000;
-tp->tv_nsec = (real_us % 1000000) * 1000;
-last_measure = m;
+    uint64_t real_us = (uint64_t) (m + rollover * micro_rollover_useconds);
+    tp->tv_sec = real_us / 1000000;
+    tp->tv_nsec = (real_us % 1000000) * 1000;
+    last_measure = m;
 
-return 0;
+    return 0;
 }
+
+#endif  // ifndef WITH_POSIX
