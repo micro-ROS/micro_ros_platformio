2e496587f753937d43f542edd6ca716f7e7235be
Błażej Sowa <bsowa123@gmail.com>
2022-09-20 15:06:35 +0200

    Fix issues with clock_gettime implementation
    - Fix race condition in rollover detection by disabling interrupts
    - Fix integer overflows by casting types in the correct order

--- platform_code/stm32cube/clock_gettime.cpp
+++ platform_code/stm32cube/clock_gettime.cpp
@@ -1,6 +1,18 @@
 #include <sys/time.h>
 
-extern "C" uint32_t HAL_GetTick();
+#if defined(STM32F0xx)
+#include "stm32f0xx_hal.h"
+#elif defined(STM32F1xx)
+#include "stm32f1xx_hal.h"
+#elif defined(STM32F2xx)
+#include "stm32f2xx_hal.h"
+#elif defined(STM32F3xx)
+#include "stm32f3xx_hal.h"
+#elif defined(STM32F4xx)
+#include "stm32f4xx_hal.h"
+#elif defined(STM32F7xx)
+#include "stm32f7xx_hal.h"
+#endif
 
 constexpr uint64_t GETTICK_ROLLOVER_USECONDS = 4294967296000UL;
 
@@ -9,14 +21,16 @@ extern "C" int clock_gettime(clockid_t unused, struct timespec *tp) {
   static uint32_t rollover = 0;
   static uint32_t last_measure = 0;
 
+  __disable_irq();
   uint32_t m = HAL_GetTick();
   rollover += (m < last_measure) ? 1 : 0;
+  last_measure = m;
+  __enable_irq();
 
   uint64_t real_us =
-      (uint64_t)(m * 1000 + rollover * GETTICK_ROLLOVER_USECONDS);
+      static_cast<uint64_t>(m) * 1000UL + rollover * GETTICK_ROLLOVER_USECONDS;
   tp->tv_sec = real_us / 1000000;
   tp->tv_nsec = (real_us % 1000000) * 1000;
-  last_measure = m;
 
   return 0;
 }
