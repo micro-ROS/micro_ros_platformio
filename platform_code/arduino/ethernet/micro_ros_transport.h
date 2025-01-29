19325c552ac9c894bd1bfa88793eb0aeea42f20a
Patrick Wspanialy <patrick.wspanialy@ecoation.com>
2025-01-29 12:56:09 -0500

    wip

--- platform_code/arduino/ethernet/micro_ros_transport.h
+++ platform_code/arduino/ethernet/micro_ros_transport.h
@@ -7,23 +7,45 @@ struct micro_ros_agent_locator {
     IPAddress gateway;    // Gateway IP address
     const char* hostname; // Device hostname
     int port;            // Agent port
+    
+    // Optional event callbacks
+    void (*on_eth_connected)() = nullptr;
+    void (*on_eth_disconnected)() = nullptr;
+    void (*on_eth_got_ip)(IPAddress ip) = nullptr;
 };
 
 static void ethernet_event_handler(WiFiEvent_t event) {
+    auto* locator = (micro_ros_agent_locator*)transport_args;
+    if (!locator) return;
+
     switch (event) {
         case ARDUINO_EVENT_ETH_START:
-            if (((micro_ros_agent_locator*)transport_args)->hostname != nullptr) {
-                ETH.setHostname(((micro_ros_agent_locator*)transport_args)->hostname);
+            if (locator->hostname != nullptr) {
+                ETH.setHostname(locator->hostname);
             }
             break;
+            
         case ARDUINO_EVENT_ETH_CONNECTED:
+            if (locator->on_eth_connected) {
+                locator->on_eth_connected();
+            }
             break;
+            
         case ARDUINO_EVENT_ETH_GOT_IP:
+            if (locator->on_eth_got_ip) {
+                locator->on_eth_got_ip(ETH.localIP());
+            }
             break;
+            
         case ARDUINO_EVENT_ETH_DISCONNECTED:
+            if (locator->on_eth_disconnected) {
+                locator->on_eth_disconnected();
+            }
             break;
+            
         case ARDUINO_EVENT_ETH_STOP:
             break;
+            
         default:
             break;
     }
@@ -35,7 +57,10 @@ static inline void set_microros_ethernet_transports(
     IPAddress netmask,
     IPAddress agent_ip, 
     uint16_t agent_port,
-    const char* hostname = nullptr
+    const char* hostname = nullptr,
+    void (*on_connected)() = nullptr,
+    void (*on_disconnected)() = nullptr,
+    void (*on_got_ip)(IPAddress ip) = nullptr
 ) {
     static struct micro_ros_agent_locator locator;
 
@@ -48,6 +73,11 @@ static inline void set_microros_ethernet_transports(
     locator.gateway = gateway;
     locator.hostname = hostname;
     locator.port = agent_port;
+    
+    // Store optional callbacks
+    locator.on_eth_connected = on_connected;
+    locator.on_eth_disconnected = on_disconnected;
+    locator.on_eth_got_ip = on_got_ip;
 
     rmw_uros_set_custom_transport(
         false,
